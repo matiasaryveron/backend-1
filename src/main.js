9f54d0f400b356b683d50f17ac0ace197020b0e8
Matias Ary Veron <veron-matias@hotmail.com>
2024-06-24 19:44:00 -0300

    MatiasAryVeron

--- src/main.js
+++ src/main.js
@@ -1,17 +1,46 @@
 import express from "express"
 import productsRouter from "./routes/products.router.js"
 import cartsRouter from "./routes/carts.router.js"
+import { engine } from "express-handlebars"
+import { Server } from "socket.io"
+import viewsRouter from "./routes/views.router.js"
+import ProductManager from "./controllers/product-manager.js"
 
 const app = express()
 const PORT = 8080;
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static ("./src/public"))
+
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views","./src/views")
 
 app.use("/api/products", productsRouter)
 app.use ("/api/carts",cartsRouter)
+app.use("/", viewsRouter)
 
 app.use(express.static("./src/public"))
 
-app.listen(PORT, () => {
+const httpServer = app.listen(PORT, () => {
     console.log (`Escuchando en el puerto ${PORT}`)
+})
+
+const productManager = new ProductManager ("./src/models/productos.json")
+
+const io = new Server (httpServer)
+
+io.on ("connection", async (socket) => {
+    console.log ("cliente conectado")
+
+    socket.emit("productos", await productManager.getProducts());
+    socket.on("eliminarProducto", async (id) =>{
+        await productManager.deleteProduct(id);
+        io.socket.emit("productos", await productManager.getProducts())
+
+    })
+    socket.on("agregarProducto", async (producto) =>{
+        await productManager.addProduct(producto)
+        io.socket.emit("productos", await productManager.getProducts())
+    })
 })
