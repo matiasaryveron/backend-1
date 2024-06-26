82107a1e5c3384fd6606fe7250e64333618fa072
Matias Ary Veron <veron-matias@hotmail.com>
2024-06-26 13:26:58 -0300

    MatiasAryVeron

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import ProductManager from "../controllers/product-manager.js"
 
 const router = Router()
 
@@ -6,8 +7,17 @@ router.get ("/realtimeproducts", async (req, res) =>{
     res.render ("realtimeproducts")
 })
 
-router.get("/productos", async (req, res) => {
-    res.render("home", {});
+const productManager = new ProductManager("./src/models/productos.json")
+
+router.get("/", async (req, res) => {
+    try {
+        const productos = await productManager.getProducts();
+
+        res.render("home", {productos})
+
+    } catch (error) {
+        res.status(500).send("Error al intentar obtener productos")
+    }
 })
 
 export default router
