b6c3cb3d332491217ad54c3edc1539dc1d6f2158
Matias Ary Veron <veron-matias@hotmail.com>
2024-07-17 15:22:49 -0300

    matiasAryVeron

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,23 +1,65 @@
-import { Router } from "express";
-import ProductManager from "../controllers/product-manager.js"
+import express from "express";
+const router = express.Router();
+import ProductManager from "../dao/db/product-manager-db.js";
+import CartManager from "../dao/db/cart-manager-db.js";
 
-const router = Router()
+const productManager = new ProductManager();
+const cartManager = new CartManager();
 
-router.get ("/realtimeproducts", async (req, res) =>{
-    res.render ("realtimeproducts")
-})
 
-const productManager = new ProductManager("./src/models/productos.json")
+router.get("/products", async (req, res) => {
+try {
+    const { page = 1, limit = 2 } = req.query;
+    const productos = await productManager.getProducts({
+        page: parseInt(page),
+        limit: parseInt(limit)
+    });
 
-router.get("/", async (req, res) => {
-    try {
-        const productos = await productManager.getProducts();
+    const nuevoArray = productos.docs.map(producto => {
+        const { _id, ...rest } = producto.toObject();
+        return rest;
+    });
 
-        res.render("home", {productos})
+    res.render("products", {
+        productos: nuevoArray,
+        hasPrevPage: productos.hasPrevPage,
+        hasNextPage: productos.hasNextPage,
+        prevPage: productos.prevPage,
+        nextPage: productos.nextPage,
+        currentPage: productos.page,
+        totalPages: productos.totalPages
+    });
 
-    } catch (error) {
-        res.status(500).send("Error al intentar obtener productos")
+} catch (error) {
+    console.error("Error al obtener productos", error);
+    res.status(500).json({
+        status: 'error',
+        error: "Error interno del servidor"
+    });
+}
+});
+
+router.get("/carts/:cid", async (req, res) => {
+const cartId = req.params.cid;
+
+try {
+    const carrito = await cartManager.getCarritoById(cartId);
+
+    if (!carrito) {
+        console.log("No existe ese carrito con el id");
+        return res.status(404).json({ error: "Carrito no encontrado" });
     }
-})
 
-export default router
+    const productosEnCarrito = carrito.products.map(item => ({
+        product: item.product.toObject(), 
+    quantity: item.quantity
+    }));
+
+    res.render("carts", { productos: productosEnCarrito });
+} catch (error) {
+    console.error("Error al obtener el carrito", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+}
+});
+
+export default router;
