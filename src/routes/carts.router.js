b3ef61e3dcd1a6ae05c4a37be62c265f72f0e714
Matias Ary Veron <veron-matias@hotmail.com>
2024-07-22 10:25:51 -0300

    Entregafinal

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -51,4 +51,93 @@ router.post("/:cid/product/:pid", async (req, res) => {
     }
 });
 
+
+router.delete("/:cid/products/:pid", async (req, res) => {
+
+    const cartId = req.params.cid;
+    const productId = req.params.pid;
+
+    try {
+        const actualizarCarrito = await cartManager.eliminarProductoDelCarrito(cartId, productId);
+        if (actualizarCarrito) {
+            res.json(actualizarCarrito.products);
+        } else {
+            res.status(404).json({ error: "Carrito o producto no encontrado" });
+        }
+    } catch (error) {
+        console.log("Error al eliminar producto", error);
+        res.status(500).json({ error: "Error interno del servidor" });
+    }
+});
+
+router.put("/:cid", async (req, res) => {
+
+    const cartId = req.params.cid;
+    const productos = req.body.products;
+
+    try {
+        const actualizarCarrito = await cartManager.actualizarCarrito(cartId, productos);
+        if (actualizarCarrito) {
+            res.json(actualizarCarrito.products);
+        } else {
+            res.status(404).json({ error: "Carrito no encontrado" });
+        }
+    } catch (error) {
+        console.log("Error al actualizar el carrito", error);
+        res.status(500).json({ error: "Error interno del servidor" });
+    }
+});
+
+router.put("/:cid/products/:pid", async (req, res) => {
+
+    const cartId = req.params.cid;
+    const productId = req.params.pid;
+    const quantity = req.body.quantity;
+
+    try {
+        const actualizarCarrito = await cartManager.actualizarCantidadProducto(cartId, productId, quantity);
+        if (actualizarCarrito) {
+            res.json(actualizarCarrito.products);
+        } else {
+            res.status(404).json({ error: "Carrito o producto no encontrado" });
+        }
+    } catch (error) {
+        console.log("No se pudo actualizar cantidad", error);
+        res.status(500).json({ error: "Error interno del servidor" });
+    }
+});
+
+router.delete("/:cid", async (req, res) => {
+    
+    const cartId = req.params.cid;
+
+    try {
+        const actualizarCarrito = await cartManager.eliminarTodosLosProductos(cartId);
+        if (actualizarCarrito) {
+            res.json(actualizarCarrito.products);
+        } else {
+            res.status(404).json({ error: "Carrito no encontrado" });
+        }
+    } catch (error) {
+        console.error("No se pudo eliminar ccarrito", error);
+        res.status(500).json({ error: "Error interno del servidor" });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default router;
