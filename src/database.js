f671eea5daf7cf0ce2fe404bf5cd1e2a65f72f4a
Matias Ary Veron <veron-matias@hotmail.com>
2024-07-23 20:56:46 -0300

    Entrega

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://matiasaryveron:nomeacuerdo33@cluster0.ncwjbkj.mongodb.net/?ecommerceretryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://matiasaryveron:nomeacuerdo33@cluster0.ncwjbkj.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0")
     .then( () => console.log("Base conectada"))
     .catch( (error)=> console.log("Error", error))
